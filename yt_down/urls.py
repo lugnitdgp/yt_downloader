827a99e4358b9c9eb01ed770d9125e79c46b73c1
Phantsure <samparksharma2000@gmail.com>
2019-12-09 20:02:22 +0530

    updated templates

--- yt_down/urls.py
+++ yt_down/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from .views import signup
+from .views import signup, login_user, get_download
 
 urlpatterns = [
-    path('signup', signup)
+    path('signup', signup), 
+    path('login', login_user),
+    path('download', get_download),
 ]
