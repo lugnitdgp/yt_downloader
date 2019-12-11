32d50186fcae3559b21ee16c30f3f51f1b3677b5
himanshu272 <himanshu.shekharjha.272@gmail.com>
2019-12-11 19:46:46 +0530

    [ADD] Profile page

--- yt_down/urls.py
+++ yt_down/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from .views import signup, login_user, get_download, home, logout_user
+from .views import signup, login_user, get_download, home, logout_user, profile
 
 urlpatterns = [
     path('signup', signup, name='signup'),
     path('login', login_user, name='login'),
     path('download', get_download, name='download'),
-    path('logout', logout_user, name='logout')
+    path('logout', logout_user, name='logout'),
+    path('profile', profile, name="profile"),
 ]
