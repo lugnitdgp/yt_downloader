66d9e9797771250a392749cf9505e94b69e34942
himanshu272 <himanshu.shekharjha.272@gmail.com>
2019-12-11 12:45:11 +0530

    [ADD] Home page view and some fix

--- yt_down/urls.py
+++ yt_down/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from .views import signup, login_user, get_download, home
+from .views import signup, login_user, get_download, home, logout_user
 
 urlpatterns = [
-    path('signup', signup),
-    path('login', login_user),
-    path('download', get_download),
-    path('home', home)
+    path('signup', signup, name='signup'),
+    path('login', login_user, name='login'),
+    path('download', get_download, name='download'),
+    path('logout', logout_user, name='logout')
 ]
