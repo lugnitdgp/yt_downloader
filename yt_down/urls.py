2c48323f20dbed195229d8d1ae551127ff1c2423
himanshu272 <himanshu.shekharjha.272@gmail.com>
2019-12-12 20:33:20 +0530

    [ADD] Basic player and redirecting

--- yt_down/urls.py
+++ yt_down/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from .views import signup, login_user, get_download, home, logout_user, profile
+from .views import signup, login_user, get_download, home, logout_user, profile, play_video
 
 urlpatterns = [
     path('signup', signup, name='signup'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('download', get_download, name='download'),
     path('logout', logout_user, name='logout'),
     path('profile', profile, name="profile"),
+    path('video/<int:id>', play_video, name="play_video")
 ]
