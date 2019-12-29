85c3f2d8c6ee5100fbbf0aac9bb0c8081eee5b05
Sampark Sharma <samparksharma2000@gmail.com>
2019-12-29 16:23:42 +0530

    Update settings.py

--- downloader/settings.py
+++ downloader/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +20,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "SA"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
