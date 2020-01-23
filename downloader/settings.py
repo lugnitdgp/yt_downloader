5cf5e275e673695a0877bb1c8a1b77cc7ecf3457
Rishab Dugar <rishabdugar.jain@gmail.com>
2020-01-23 18:01:17 +0530

    Separated SECURITY KEY to .env file and solved the " debug turned on " problem
    I have used decouple and config .
    The security key and debug bool value can now be acccessed through th .env file

--- downloader/settings.py
+++ downloader/settings.py
@@ -11,21 +11,22 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR, '')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "SA"
-
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
+
 
 
 # Application definition
