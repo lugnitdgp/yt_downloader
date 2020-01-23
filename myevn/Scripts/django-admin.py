8a92d5752ebedeec6d11b7c76cb6524fd95e4339
Rishab Dugar <56007479+kingrishabdugar@users.noreply.github.com>
2020-01-23 17:18:30 +0530

    Delete django-admin.py

--- myevn/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\rishab_dugar\documents\github\yt_downloader\myevn\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
