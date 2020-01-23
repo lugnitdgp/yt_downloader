b0ae172d34558d9fdf9b718416a12538a9aa8502
Sampark Sharma <samparksharma2000@gmail.com>
2020-01-23 21:58:22 +0530

    Revert "SEPARATED Security Key to .env file and resolved the "debug always turned on in production" problem using decouple and config  (#16)"
    This reverts commit 14613993c2e9941f96f8cc5119ae64be8e6fdc9f.

--- downloader/settings.py
+++ downloader/settings.py
@@ -11,21 +11,21 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_DIR = os.path.join(BASE_DIR, '')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', cast=bool)
-# SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = []
+SECRET_KEY = "SA"
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 
 # Application definition
